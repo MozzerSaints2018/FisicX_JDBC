7c3bbe15b0f8080adfceeb098b56a014e06456c0
MozzerSaints2018 <gpossemozer2012@gmail.com>
2019-03-04 10:49:29 -0300

    Insert Data in JDBC Fixed

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.text.SimpleDateFormat;
@@ -32,36 +33,36 @@ public class Program {
 			System.out.println("---------------------DADOS PESSOAIS-----------------------");
 
 			System.out.print("Nome: ");
-			String nome = sc.next();
+			String nome = sc.nextLine();
 			
 			System.out.print("Endereço: ");
-			String endereco = sc.next();
+			String endereco = sc.nextLine();
 			
 			System.out.print("Bairro: ");
-			String bairro = sc.next();
+			String bairro = sc.nextLine();
 			
 			System.out.print("Cidade: ");
-			String cidade = sc.next();
+			String cidade = sc.nextLine();
 			
 			System.out.print("CEP: ");
-			String cep = sc.next();
+			String cep = sc.nextLine();
 			
 			System.out.print("CPF: ");
-			String cpf = sc.next();
+			String cpf = sc.nextLine();
 			
 			System.out.print("Data de Nascimento: ");			
-			String dataNascimento = sc.next();
+			String dataNascimento = sc.nextLine();
 			
 			System.out.print("Login: ");
-			String login = sc.next();
+			String login = sc.nextLine();
 			
 			System.out.println("Senha: ");
-			String senha = sc.next();
+			String senha = sc.nextLine();
 			
 			
 			st = conn.prepareStatement(
 					"INSERT INTO FX_Pessoa"
-					+ "(pes_Nome, pes_Endereco, pes_Bairro, pes_Cidade, pes_CEP"
+					+ "(pes_Nome, pes_Endereco, pes_Bairro, pes_Cidade, pes_CEP,"
 					+ "pes_CPF, pes_DataNascimento, pes_Login, pes_Senha, pes_TipoAcesso, pes_Atibo) "
 					+ "VALUES (?,?,?,?,?,?,?,?,?,?,?)",
 					Statement.RETURN_GENERATED_KEYS );
@@ -77,6 +78,19 @@ public class Program {
 			st.setString(9, senha);
 			st.setInt(10, 1);
 			st.setInt(11, 1);
+			
+			int rowsAffected = st.executeUpdate();
+			
+			if(rowsAffected > 0) {
+				ResultSet rs = st.getGeneratedKeys(); /* Chamada de função getGeneratedKey retorna um ResultSet com o código da linha inserida.*/
+				while(rs.next()) {
+					int id = rs.getInt(1);
+					System.out.println("Done! ID: " + id);
+				}
+			}
+			else {
+				System.out.println("No rows affected");
+			}
 			
 	}
 		catch(SQLException e) {
