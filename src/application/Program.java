4f699abdb304a2ee87eeb1b95bfe6ef547eabdd3
MozzerSaints2018 <gpossemozer2012@gmail.com>
2019-03-06 16:36:12 -0300

    Login

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,18 +1,22 @@
 package application;
 
 import entitites.Aluno;
+import entitites.Login;
 import entitites.Pessoa;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		Pessoa pes = new Pessoa();
-		Aluno alu = new Aluno();
+		//Pessoa pes = new Pessoa();
+		//Aluno alu = new Aluno();
+		Login log = new Login();
 		
-		pes.inserePessoas();
-		System.out.println();
-		alu.insereAlunos();
+		log.efetuarLogin();
+		
+		//pes.inserePessoas();
+		//System.out.println();
+		//alu.insereAlunos();
 		
 	}
 
---
+++ src/entitites/Login.java
@@ -0,0 +1,78 @@
+package entitites;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+import db.DB;
+
+public class Login {
+	
+	public void efetuarLogin() {
+		
+		Scanner sc = new Scanner(System.in);
+		Connection conn = null;
+		PreparedStatement stLogin = null;
+		PreparedStatement stSenha = null;
+		ResultSet rsLogin = null;
+		ResultSet rsSenha = null;
+		
+		try {
+			
+			conn = DB.getConnection();
+			
+			System.out.print("Login: ");
+			String login = sc.next();
+			
+			System.out.print("Senha: ");
+			String senha = sc.next();
+			
+			stLogin = conn.prepareStatement(
+					"SELECT pes_Login FROM FX_Pessoa"
+					+ "WHERE pes_Login LIKE ?");
+			
+			stLogin.setString(1, login);
+			
+			//rsLogin = stLogin.executeQuery(
+			//		"SELECT pes_Login FROM FX_Pessoa"
+			//		 + "WHERE pes_Login LIKE ?");
+			
+			
+			
+			stSenha = conn.prepareStatement(
+					"SELECT pes_Senha FROM FX_Pessoa"
+					+ "WHERE pes_Senha LIKE ?");
+			
+			stSenha.setString(1, senha);
+			
+			//rsSenha = stSenha.executeQuery(
+			//		"SELECT pes_Login FROM FX_Pessoa"
+			//		 + "WHERE pes_Senha LIKE " + senha);
+			
+			
+			
+			
+			while(rsLogin.next()) {
+				System.out.println(rsLogin.getString("pes_Login"));
+			}
+			
+			while(rsSenha.next()) {
+				System.out.println(rsSenha.getString("pes_Senha"));
+			}
+			
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+		}
+		finally {
+			DB.closeConnection();
+			DB.closeResultSet(rsLogin);
+			DB.closeResultSet(rsSenha);
+			DB.closeStatement(stLogin);
+			DB.closeStatement(stSenha);
+		}
+	}
+
+}
